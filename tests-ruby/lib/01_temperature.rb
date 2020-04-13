2d21f91785e1b7fcb05c00ca4602c96e1645f652
lain <fodilboumaza@gmail.com>
2020-04-13 17:47:02 +0200

    first commit

---
+++ tests-ruby/lib/01_temperature.rb
@@ -0,0 +1,13 @@
+def ftoc (f )
+f.to_i
+
+	return f = ((f - 32) / 1.8).round
+
+end
+
+def ctof (c)
+	c.to_i
+
+	return c * 1.8 + 32
+
+end
---
+++ tests-ruby/lib/02_calculator.rb
@@ -0,0 +1,27 @@
+def add (a,b)
+
+	return a+b 
+end
+
+def subtract (a,b)
+
+	return a-b
+
+end
+
+def sum (num=0.to_i)
+	sum = [num]
+ 	i = 0
+	sum.each do
+
+	i = i + sum[i]
+end
+
+	return i
+end
+
+def multiply(a, b)
+
+	return a*b
+
+end
